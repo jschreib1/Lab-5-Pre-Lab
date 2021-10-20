152ff3d8995e5e3f9483300cb003ab84fdab8344
Jeremy Schreiber <86371980+jschreib1@users.noreply.github.com>
2021-10-20 20:30:33 +0000

    Do it once

--- dc_motor.py
+++ dc_motor.py
@@ -9,11 +9,10 @@ dcMax = 13
 pwm = GPIO.PWM(pwmPin, 50) # PWM object at 50 Hz (20 ms period)
 pwm.start(0)
 try:
-  while True:
-    for dc in range(dcMin,dcMax):
-      pwm.ChangeDutyCycle(dc)
-      print(dc)
-      time.sleep(0.5)
+  for dc in range(dcMin,dcMax):
+    pwm.ChangeDutyCycle(dc)
+    print(dc)
+    time.sleep(0.5)
 except KeyboardInterrupt:
   print("bye")
   GPIO.cleanup()
