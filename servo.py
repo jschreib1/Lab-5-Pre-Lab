48f2e31b87fc5c96b71a7d9b6045167e124447f5
Jeremy Schreiber <86371980+jschreib1@users.noreply.github.com>
2021-10-20 20:42:35 +0000

    back to normal

--- servo.py
+++ servo.py
@@ -11,6 +11,7 @@ pwm = GPIO.PWM(pwmPin, 50) # PWM object at 50 Hz (20 ms period)
 pwm.start(0)
 try:
   while True:
+    '''
     pwm.ChangeDutyCycle(dcMin)
     time.sleep(2)
     pwm.ChangeDutyCycle(dcMax)
@@ -20,7 +21,6 @@ try:
       pwm.ChangeDutyCycle(dc)
       print(dc)
       time.sleep(0.5)
-      '''
 except KeyboardInterrupt:
   print("bye")
   GPIO.cleanup()
